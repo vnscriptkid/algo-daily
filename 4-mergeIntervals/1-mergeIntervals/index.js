4243836fff8717b34eae441ad177685114d70dfc
Thanh Nguyen <vnscriptkid@gmail.com>
2021-08-26 15:23:34 +0700

    Update folder name to 4-mergeIntervals

---
+++ 4-mergeIntervals/1-mergeIntervals/index.js
@@ -0,0 +1,40 @@
+// 0 1 2 3 4 5 6 7 8 9 10
+//   [     ]
+//       [     ]
+//               [   ]
+//                 [    ]
+
+function merge(intervals) {
+  // sort by start time
+  intervals.sort((a, b) => a[0] - b[0])
+  const result = []
+
+  let prev = [...intervals[0]] // avoid modifying original interval
+
+  for (let i = 1; i < intervals.length; i++) {
+    let cur = intervals[i]
+
+    // ]  {
+    if (cur[0] > prev[1]) {
+      result.push(prev)
+      prev = [...cur]
+    }
+    // _{_]_ => merge
+    //// _{_]__}_
+    //// _{_}_]_
+    else prev[1] = Math.max(cur[1], prev[1])
+  }
+
+  result.push(prev)
+
+  return result
+}
+
+const intervals = [
+  [6, 7],
+  [2, 4],
+  [5, 9],
+]
+const result = merge(intervals)
+
+module.exports = {merge}
