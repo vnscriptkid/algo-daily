a6a4f1a88ee885ee47f1b73eec3be1e8e1a6187d
Thanh Nguyen <vnscriptkid@gmail.com>
2021-08-27 15:41:15 +0700

    Solved cyclic sort

--- 5-cyclicSortPattern/1-cyclicSort/index.js
+++ 5-cyclicSortPattern/1-cyclicSort/index.js
@@ -1,7 +1,24 @@
-// Problem:
+// Given an unsorted array containing n distinct numbers taken from 1, 2, …, n
+// Sort the array in-place
+
+// 0  1  2  3  4
+// 1, 2, 3, 4, 5
+//               ^
+
+function swap(arr, x, y) {
+  ;[arr[y], arr[x]] = [arr[x], arr[y]]
+}
 
 function sort(arr) {
-  return arr
+  let i = 0
+  while (i < arr.length) {
+    if (arr[i] === i + 1) {
+      // i'm at correct position
+      i++
+    } else {
+      swap(arr, i, arr[i] - 1)
+    }
+  }
 }
 
 module.exports = {sort}
