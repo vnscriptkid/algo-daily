43212596e6bd77a0ff9fd54c06bb6ce2c7f19302
Thanh Nguyen <vnscriptkid@gmail.com>
2021-09-05 10:47:27 +0700

    Solved two single numbers

--- 12-bitManipulation/2-twoSingleNumbers/index.js
+++ 12-bitManipulation/2-twoSingleNumbers/index.js
@@ -1,28 +1,28 @@
 // Two Single Numbers
-// In a non-empty array of numbers, every number appears exactly twice except two numbers that appear only once. Find the two numbers that appear only once.
+// In a non-empty array of numbers, every number appears exactly twice except two numbers that appear only once.
+// Find the two numbers that appear only once.
+// Input: [1, 4, 2, 1, 3, 5, 6, 2, 3, 5]
+// 4 ^ 6
+// Output: [4, 6]
 
 function findTwoSingleNumbers(arr) {
-  // xor all together to find axb
   let axb = 0
 
   for (let num of arr) {
     axb = axb ^ num
   }
 
-  // we got axb now
-  // a !== b, at least 1 bit different (bit 1)
-  // find rightMostSetBit
+  // find first set (1) from the right
   let c = 1
   while ((c & axb) === 0) {
     c = c << 1
   }
 
-  // partition in 2 groups
+  // use c to partition arr into 2 groups
   let [x, y] = [0, 0]
 
   for (let num of arr) {
-    if ((c & num) === 0) {
-      // bit is 0
+    if ((num & c) === 0) {
       x = x ^ num
     } else {
       y = y ^ num
