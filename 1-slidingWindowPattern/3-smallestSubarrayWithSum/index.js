15ff31722df659f80d0fdc7f18e0d7687710fce7
Thanh Nguyen <vnscriptkid@gmail.com>
2021-09-09 18:21:17 +0700

    Solved smallest subset sum

--- 1-slidingWindowPattern/3-smallestSubarrayWithSum/index.js
+++ 1-slidingWindowPattern/3-smallestSubarrayWithSum/index.js
@@ -1,53 +1,30 @@
-// [2, 1, 5, 2, 3, 2], S=7
-//        ^  $
-//        [  ]
+// Given an array of positive numbers and a positive number ‘S’, find the length of the smallest contiguous subarray whose sum is greater than or equal to ‘S’. Return 0, if no such subarray exists.
 
-// curSum = 7
+// [2, 1, 5, 2, 3, 2], S=7
+//              [  ]
+//              @     ^
+
+// curSum = 5
 // minLength = 2
 
 function findSmallestSubarray(arr, sum) {
   let windowStart = 0
   let curSum = 0
-
-  // for (let windowEnd = 0; windowEnd < arr.length; windowEnd++) {
-  //     curSum += arr[windowEnd];
-  // }
   let minLength = Infinity
 
-  //   let windowEnd = 0
-  //   while (windowEnd >= arr.length) {
-  //     if (curSum < sum) {
-  //       windowEnd++
-  //       curSum += arr[windowEnd]
-  //     } else {
-  //       // curSum >= sum
-  //       const curLength = windowEnd - windowStart + 1
-  //       minLength = Math.min(minLength, curLength)
-
-  //       // can we find smaller window
-  //       curSum -= arr[windowStart]
-  //       windowStart++
-  //       // TODO: Edge case windowStart === windowEnd
-  //     }
-  //   }
-
   for (let windowEnd = 0; windowEnd < arr.length; windowEnd++) {
-    // expanding window
     curSum += arr[windowEnd]
 
-    if (curSum < sum) continue // to expand window
-
-    // curSum >= sum
-    // can we have smaller subarray
-    do {
-      const curLength = windowEnd - windowStart + 1
-      minLength = Math.min(minLength, curLength)
+    while (curSum >= sum) {
+      let curSize = windowEnd - windowStart + 1
+      minLength = Math.min(minLength, curSize)
+      // shrink down the window
       curSum -= arr[windowStart]
       windowStart++
-    } while (curSum >= sum)
+    }
   }
 
-  return minLength
+  return minLength === Infinity ? 0 : minLength
 }
 
 module.exports = {findSmallestSubarray}
