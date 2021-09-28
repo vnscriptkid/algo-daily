a9113f6deec53c23969c914643684ee3ef051ef5
Thanh Nguyen <vnscriptkid@gmail.com>
2021-09-28 19:13:50 +0700

    Solved cyclic sort again

--- 5-cyclicSortPattern/4-theDuplicateNumber/index.js
+++ 5-cyclicSortPattern/4-theDuplicateNumber/index.js
@@ -1,31 +1,34 @@
-// [1, 2, 3, 4, 5]
-// [1, 3, 4, 4, 2]
-//        ^
+// Find the Duplicate Number (easy)
+// We are given an unsorted array containing ‘n+1’ numbers taken from the range 1 to ‘n’. The array has only one duplicate but it can be repeated multiple times. Find that duplicate number without using any extra space. You are, however, allowed to modify the input array.
 
-// [1, 2, 3, 4, 5]
-// [4, 2, 1, 4, 4]
-//              ^
+//        [1, 2, 3, 4]
+// Input: [1, 4, 3, 4, 2]
+//            ^
+// Output: 4
+
+//        [1, 2, 3, 4, 5]
+// Input: [1, 2, 3, 3, 5, 4]
+//                  ^
+
+// Output: 3
+
+//        [1, 2, 3, 4]
+// Input: [4, 2, 1, 4, 4]
+//         ^
+// Output: 4
 
 function findDuplicateNumber(nums) {
   let i = 0
-
   while (i < nums.length) {
     let num = nums[i]
     let correctIndex = num - 1
 
     if (i !== correctIndex) {
-      if (nums[correctIndex] === correctIndex + 1) return num
-      else {
+      if (nums[correctIndex] !== correctIndex + 1) {
         ;[nums[i], nums[correctIndex]] = [nums[correctIndex], nums[i]]
-      }
+      } else return num
     } else i++
   }
-
-  for (let i = 0; i < nums.length; i++) {
-    if (nums[i] !== i + 1) return nums[i]
-  }
-
-  return -1
 }
 
 module.exports = {findDuplicateNumber}
