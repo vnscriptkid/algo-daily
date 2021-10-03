e38b705a95894bc6e35ed7b8c43ca31032ed463c
Thanh Nguyen <vnscriptkid@gmail.com>
2021-10-03 15:14:48 +0700

    Solved Searching in sorted infinite array

--- 11-modifiedBinarySearch/4-infiniteArray/index.js
+++ 11-modifiedBinarySearch/4-infiniteArray/index.js
@@ -1,12 +1,21 @@
+// Search in a Sorted Infinite Array
+
+// Given an infinite sorted array (or an array with unknown size), find if a given number ‘key’ is present in the array. Write a function to return the index of the ‘key’ if it is present in the array, otherwise return -1.
+
+// Since it is not possible to define an array with infinite (unknown) size, you will be provided with an interface ArrayReader to read elements of the array. ArrayReader.get(index) will return the number at index; if the array’s size is smaller than the index, it will return Integer.MAX_VALUE.
+
+// Input: [4, 6, 8, 10, 12, 14, 16, 18, 20, 22, 24, 26, 28, 30], key = 16
+//               $           ^
+
 function search(infiniteArr, key) {
-  // find bound of key
+  // find search space first
   let left = 0,
     right = 1
+
   while (key > infiniteArr.get(right)) {
-    let boundSize = right - left + 1
-    // expand bound to the right
+    let sizeOfCurrentBound = right - left + 1
     left = right + 1
-    right += boundSize * 2
+    right += sizeOfCurrentBound * 2
   }
 
   return binarySearch(infiniteArr, left, right, key)
