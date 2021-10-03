4ebe4e4a08b35d3ddc106ade16047b4db946f9a8
Thanh Nguyen <vnscriptkid@gmail.com>
2021-10-03 14:15:31 +0700

    Solved 4-stringPermutationsByChangingCase dfs

--- 10-subsets/4-stringPermutationsByChangingCase/bfs.js
+++ 10-subsets/4-stringPermutationsByChangingCase/bfs.js
@@ -1,7 +1,14 @@
+// String Permutations by changing case
+// Given a string, find all of its permutations preserving the character sequence but changing case.
+
+// Input: "ab7c"
+// Output: "ab7c", "Ab7c", "aB7c", "AB7c", "ab7C", "Ab7C", "aB7C", "AB7C"
+
 //            [ab7c]
 // a:     [ab7c     Ab7c]
 // b: [ab7c, Ab7c, aB7c, AB7c]
 // 7: []
+// c:
 
 function findCasePermutations(str) {
   const queue = [str]
--- 10-subsets/4-stringPermutationsByChangingCase/dfs.js
+++ 10-subsets/4-stringPermutationsByChangingCase/dfs.js
@@ -1,19 +1,39 @@
-function findCasePermutations(str, cur = [], all = []) {
-  if (cur.length === str.length) {
-    all.push(cur.join(''))
+// String Permutations by changing case
+// Given a string, find all of its permutations preserving the character sequence but changing case.
+
+// Input: "ab7c"
+// Output: "ab7c", "Ab7c", "aB7c", "AB7c", "ab7C", "Ab7C", "aB7C", "AB7C"
+
+//             ''
+// a:      a           A
+
+// b:   ab    aB    Ab    AB
+
+// 7    ab7   aB7   Ab7   AB7
+
+// c   ab7c  ab7C   aB7c   aB7C    Ab7c     Ab7C    AB7c    AB7C
+
+function findCasePermutations(str, curPer = '', all = []) {
+  if (curPer.length === str.length) {
+    // we're done
+    all.push(curPer)
     return
   }
 
-  let curChar = str.charAt(cur.length)
+  let curChar = str.charAt(curPer.length)
 
-  cur.push(curChar)
-  findCasePermutations(str, cur, all)
-  cur.pop()
-
+  // case 1: number
+  // case 2: char
+  const choices = []
+  choices.push(curChar)
   if (curChar.match(/[a-z]/)) {
-    cur.push(curChar.toUpperCase())
-    findCasePermutations(str, cur, all)
-    cur.pop()
+    choices.push(curChar.toUpperCase())
+  }
+
+  for (let choice of choices) {
+    curPer += choice
+    findCasePermutations(str, curPer, all)
+    curPer = curPer.slice(0, curPer.length - 1)
   }
 
   return all
