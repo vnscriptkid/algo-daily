a4a9513e0c9b9f480fcf093f02a559194d86f960
Thanh Nguyen <vnscriptkid@gmail.com>
2021-10-04 09:06:18 +0700

    Solved String Permutations by changing case bfs

--- 10-subsets/4-stringPermutationsByChangingCase/bfs.js
+++ 10-subsets/4-stringPermutationsByChangingCase/bfs.js
@@ -4,11 +4,15 @@
 // Input: "ab7c"
 // Output: "ab7c", "Ab7c", "aB7c", "AB7c", "ab7C", "Ab7C", "aB7C", "AB7C"
 
-//            [ab7c]
-// a:     [ab7c     Ab7c]
-// b: [ab7c, Ab7c, aB7c, AB7c]
-// 7: []
-// c:
+//       [ab7c]
+
+//  [ab7c       Ab7c]
+
+// [ab7c  Ab7c aB7c AB7c]
+
+// [ab7c  Ab7c aB7c AB7c]
+
+// [ab7c  Ab7c aB7c AB7c ab7C Ab7C aB7C AB7C]
 
 function findCasePermutations(str) {
   const queue = [str]
@@ -18,13 +22,14 @@ function findCasePermutations(str) {
 
     if (!curChar.match(/[a-z]/)) continue
 
-    let queueSizeNow = queue.length
-
-    for (let j = 0; j < queueSizeNow; j++) {
+    let queueSize = queue.length
+    let j = 0
+    while (j < queueSize) {
       let curPer = queue[j]
       let newPer =
         curPer.slice(0, i) + curChar.toUpperCase() + curPer.slice(i + 1)
       queue.push(newPer)
+      j++
     }
   }
 
