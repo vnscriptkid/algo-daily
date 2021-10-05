913be3a82623cd371112d8e1a8088137fd51186d
Thanh Nguyen <vnscriptkid@gmail.com>
2021-10-05 18:15:39 +0700

    Solved missing number

--- 12-bitManipulation/4-missingNumber/index.js
+++ 12-bitManipulation/4-missingNumber/index.js
@@ -1,12 +1,23 @@
+// Given an array of n-1 integers in the range from 1 to n,
+// find the one number that is missing from the array.
+
+// Input: 1, 5, 2, 6, 4
+
+// 3
+
+// Answer: 3
+
+// a ^ a = 0
+
 function findMissingNumber(arr) {
   let x1 = 0
   for (let num of arr) {
-    x1 = x1 ^ num
+    x1 ^= num
   }
 
   let x2 = 0
   for (let i = 1; i <= arr.length + 1; i++) {
-    x2 = x2 ^ i
+    x2 ^= i
   }
 
   return x1 ^ x2
