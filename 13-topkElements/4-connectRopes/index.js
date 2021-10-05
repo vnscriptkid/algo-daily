5a2ece6ac0996d07b827a68cfcf62123755e4e64
Thanh Nguyen <vnscriptkid@gmail.com>
2021-10-05 18:36:55 +0700

    Solved connect ropes

--- 13-topkElements/4-connectRopes/index.js
+++ 13-topkElements/4-connectRopes/index.js
@@ -1,30 +1,30 @@
 // Connect Ropes
 
-const Heap = require('collections/heap')
-
 // Given ‘N’ ropes with different lengths, we need to connect these ropes into one big rope with minimum cost.
 // The cost of connecting two ropes is equal to the sum of their lengths.
 
 // Input: [1, 3, 11, 5, 2]
 // Output: 42
 
-// [(11), 11]
+// minHeap: 22
+// connect: 11 + 11 = 22
+// cost: 42
 
-// first: 5
-// sec: 6
-// connect: 11
+const Heap = require('collections/heap')
 
 function minimizeRopeCost(ropes = []) {
-  const minHeap = new Heap(ropes, null, (a, b) => b - a)
+  const minHeap = new Heap([], null, (a, b) => b - a)
+
+  for (let rope of ropes) {
+    minHeap.add(rope)
+  }
 
   let cost = 0
 
   while (minHeap.length > 1) {
-    const combined = minHeap.pop() + minHeap.pop()
-
-    cost += combined
-
-    minHeap.add(combined)
+    let newRope = minHeap.pop() + minHeap.pop()
+    minHeap.add(newRope)
+    cost += newRope
   }
 
   return cost
