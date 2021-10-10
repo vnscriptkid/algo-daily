6a3b1b213e66fe5935ddd4a21dec4960e69aefcf
Thanh Nguyen <vnscriptkid@gmail.com>
2021-10-11 01:49:57 +0700

    Solved Minimum Subset Sum Difference bottom up

--- 15-01knapsack/4-minSubsetSumDiff/bottomUp.js
+++ 15-01knapsack/4-minSubsetSumDiff/bottomUp.js
@@ -1,3 +1,21 @@
+// Minimum Subset Sum Difference
+// Given a set of positive numbers, partition the set into two subsets with minimum difference between their subset sums.
+
+// 10
+// {5} {5} => 0
+
+// Input: {1, 2, 3, 9} => 15 = 6 + 9
+
+//    0  1  2  3  4  5  6  7
+// 1  T  T  F  F  F  F  F  F
+// 2  T  T  T  T  F  F  F  F
+// 3  T  T  T  T  T  T  T  F
+// 9  T  T  T  T  T  T  T  F
+
+// Output: 3
+// Explanation: We can partition the given set into two subsets where minimum absolute difference
+// between the sum of numbers is '3'. Following are the two subsets: {1, 2, 3} & {9}.
+
 function createMatrix(rows, cols) {
   return Array(rows)
     .fill(null)
@@ -5,47 +23,38 @@ function createMatrix(rows, cols) {
 }
 
 function findMinSubsetSumDiff(numbers) {
-  const sum = numbers.reduce((a, c) => a + c, 0)
-  const target = Math.ceil(sum / 2)
-
   const numOfRows = numbers.length
-  const numOfCols = target + 1
+  const sum = numbers.reduce((a, i) => a + i, 0)
+  const numOfCols = Math.floor(sum / 2) + 1
+
   const dp = createMatrix(numOfRows, numOfCols)
 
-  // first col of dp is all true
   for (let row of dp) {
     row[0] = true
   }
 
-  // first row, at col = nubmers[0]
   dp[0][numbers[0]] = true
 
-  for (let curRow = 1; curRow < numOfRows; curRow++) {
-    for (let curCol = 1; curCol < numOfCols; curCol++) {
-      let curNum = numbers[curRow]
-      // skip number
-      if (dp[curRow - 1][curCol]) {
-        dp[curRow][curCol] = true
+  for (let row = 1; row < numOfRows; row++) {
+    for (let col = 1; col < numOfCols; col++) {
+      if (dp[row - 1][col]) {
+        dp[row][col] = true
         continue
       }
 
-      // take number
-      if (curCol >= curNum) dp[curRow][curCol] = dp[curRow - 1][curCol - curNum]
-    }
-    if (dp[curRow][numOfCols - 1]) {
-      const complement = sum - target
-      return target - complement
-    }
-  }
-
-  for (let curCol = numOfCols - 2; curCol >= 0; curCol--) {
-    for (let curRow = 1; curRow < numOfRows; curRow++) {
-      if (dp[curRow][curCol]) {
-        const complement = sum - curCol
-        return Math.abs(curCol - complement)
+      if (col >= numbers[row]) {
+        dp[row][col] = dp[row - 1][col - numbers[row]]
       }
     }
   }
+
+  let col = numOfCols - 1
+  while (dp[numOfRows - 1][col] === false) {
+    col--
+  }
+
+  let otherSum = sum - col
+  return Math.abs(col - otherSum)
 }
 
 module.exports = {findMinSubsetSumDiff}
