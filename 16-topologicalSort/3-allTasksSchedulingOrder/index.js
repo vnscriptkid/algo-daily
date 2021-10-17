030c258075413f8bf56623cc2b6a2b6c7038375e
Thanh Nguyen <vnscriptkid@gmail.com>
2021-10-17 13:31:16 +0700

    Solved All Tasks Scheduling Orders

--- 16-topologicalSort/3-allTasksSchedulingOrder/index.js
+++ 16-topologicalSort/3-allTasksSchedulingOrder/index.js
@@ -1,6 +1,14 @@
+// All Tasks Scheduling Orders
+
+// There are ‘N’ tasks, labeled from ‘0’ to ‘N-1’. Each task can have some prerequisite tasks which need to be completed before it can be scheduled. Given the number of tasks and a list of prerequisite pairs, write a method to print all possible ordering of tasks meeting all prerequisites.
+
+// Input: Tasks=4, Prerequisites=[3, 2], [3, 0], [2, 0], [2, 1]
+// Output:
+// 1) [3, 2, 0, 1]
+// 2) [3, 2, 1, 0]
+
 function findAllSchedulingOrders(tasks, prerequisites) {
-  // build graph of deps
-  // build incommings
+  // build graph, deps map
   const graph = new Map()
   const incommings = new Map()
 
@@ -9,39 +17,39 @@ function findAllSchedulingOrders(tasks, prerequisites) {
     incommings.set(task, 0)
   }
 
-  for (let [src, dest] of prerequisites) {
-    graph.get(src).push(dest)
+  for (let [source, dest] of prerequisites) {
+    graph.get(source).push(dest)
     incommings.set(dest, incommings.get(dest) + 1)
   }
 
-  const sources = []
-
+  // build candidates
+  const candidates = []
   for (let [task, numOfDeps] of incommings.entries()) {
-    if (numOfDeps === 0) sources.push(task)
+    if (numOfDeps === 0) candidates.push(task)
   }
 
-  return dfs(sources, graph, incommings)
+  return dfs(graph, incommings, candidates)
 }
 
-function dfs(sources, graph, incommings, cur = [], all = []) {
-  if (graph.size === cur.length) {
+function dfs(graph, incommings, candidates, cur = [], all = []) {
+  // base case
+  if (cur.length === graph.size) {
     all.push([...cur])
     return
   }
 
-  for (let task of sources) {
+  for (let task of candidates) {
     cur.push(task)
 
-    const newSources = sources.filter(curTask => curTask !== task)
+    const newCandidates = candidates.filter(c => c !== task)
 
-    // update incommings
     for (let dest of graph.get(task)) {
       incommings.set(dest, incommings.get(dest) - 1)
 
-      if (incommings.get(dest) === 0) newSources.push(dest)
+      if (incommings.get(dest) === 0) newCandidates.push(dest)
     }
 
-    dfs(newSources, graph, incommings, cur, all)
+    dfs(graph, incommings, newCandidates, cur, all)
 
     // backtrack
     cur.pop()
