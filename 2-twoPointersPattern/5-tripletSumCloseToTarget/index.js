c5482128c31ea1bd3c713e578a8f05514009eb79
Thanh Nguyen <vnscriptkid@gmail.com>
2021-10-19 09:22:20 +0700

    Solved Triplet Sum Close to Target

--- 2-twoPointersPattern/5-tripletSumCloseToTarget/index.js
+++ 2-twoPointersPattern/5-tripletSumCloseToTarget/index.js
@@ -4,33 +4,44 @@
 // find a triplet in the array whose sum is as close to the target number as possible, return the sum of the triplet.
 // If there are more than one such triplet, return the sum of the triplet with the smallest sum.
 
+// Input: [-3, -1, 1, 2], target=1
+//              #
+//                    @$
+
+// curSum: 0
+// curDiff: 1
+// bestAnswer: 1
+
+// Output: 0
+// Explanation: The triplet [-3, 1, 2] has the closest sum to the target.
+
 function searchTriplets(arr, target) {
-  // sort in asending order
   arr.sort((a, b) => a - b)
 
-  let minDiff = Infinity
+  let bestDiff = Infinity
   let bestSum = Infinity
 
   for (let firstIdx = 0; firstIdx < arr.length - 2; firstIdx++) {
-    let secondIdx = firstIdx + 1
-    let thirdIdx = arr.length - 1
+    let secondIdx = firstIdx + 1,
+      thirdIdx = arr.length - 1
 
     while (secondIdx < thirdIdx) {
-      let sum = arr[firstIdx] + arr[secondIdx] + arr[thirdIdx]
+      let curSum = arr[firstIdx] + arr[secondIdx] + arr[thirdIdx]
+      let curDiff = Math.abs(curSum - target)
 
-      if (sum === target) return target
-
-      let curDiff = Math.abs(sum - target)
-      if (curDiff < minDiff) {
-        minDiff = curDiff
-        bestSum = sum
+      if (curDiff < bestDiff) {
+        bestDiff = curDiff
+        bestSum = curSum
+      } else if (curDiff === bestDiff && curSum < bestSum) {
+        bestDiff = curDiff
+        bestSum = curSum
       }
 
-      if (sum < target) secondIdx++
+      if (curSum === target) return curSum
+      else if (curSum < target) secondIdx++
       else thirdIdx--
     }
   }
-
   return bestSum
 }
 
