65662a155ae1889e4a25188ad6bfd84f2c5ef491
Thanh Nguyen <vnscriptkid@gmail.com>
2021-10-22 11:30:43 +0700

    Solved Find all Duplicate Numbers

--- 5-cyclicSortPattern/5-allDuplicateNumbers/index.js
+++ 5-cyclicSortPattern/5-allDuplicateNumbers/index.js
@@ -2,33 +2,34 @@
 // We are given an unsorted array containing ‘n’ numbers taken from the range 1 to ‘n’.
 // The array has some duplicates, find all the duplicate numbers without using any extra space.
 
-// Input: [3, 4, 4, 5, 5]
+// 0 1 2 3 4
+// 1 2 3 4 5
+
+// idx:    0  1  2  3  4
 // Input: [5, 4, 3, 4, 5]
-//                     ^
+//                        ^
+
 // Output: [4, 5]
 
 function findAllDuplicates(nums) {
-  // first iteration: place each number to it's correct index
-
+  // put back each number to its correct slot
   let i = 0
   while (i < nums.length) {
     let curNum = nums[i]
-    let correctIndex = curNum - 1
+    let correctIdx = curNum - 1
 
-    if (i !== correctIndex && curNum !== nums[correctIndex]) {
-      ;[nums[i], nums[correctIndex]] = [nums[correctIndex], nums[i]]
+    // swap when: currentNum is not at it's correct index && the number at correct index is diff from curNumber
+    if (i !== correctIdx && curNum !== nums[correctIdx]) {
+      ;[nums[i], nums[correctIdx]] = [nums[correctIdx], nums[i]]
     } else i++
   }
 
-  // second round: find duplicates
+  // finding the duplicates
   const output = []
-
   for (let i = 0; i < nums.length; i++) {
-    let curNum = nums[i]
-    let correctIndex = curNum - 1
-
-    if (i !== correctIndex) output.push(curNum)
+    if (nums[i] !== i + 1) output.push(nums[i])
   }
+
   return output
 }
 
