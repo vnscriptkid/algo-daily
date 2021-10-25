11a474c2717ca2ccdd3e5af0a63aade74b0e1e67
Thanh Nguyen <vnscriptkid@gmail.com>
2021-10-25 22:08:48 +0700

    Solved Next Interval

--- 9-twoHeaps/4-nextInterval/index.js
+++ 9-twoHeaps/4-nextInterval/index.js
@@ -6,12 +6,24 @@ for an interval ‘i’ its next interval ‘j’ will have the smallest ‘star
 Write a function to return an array containing indices of the next interval of each input interval. 
 If there is no next interval of a given interval, return -1. 
 It is given that none of the intervals have the same start point.
+
 Example 1:
 Input: Intervals [[2,3], [3,4], [5,6]]
+//                  x  ,   y  ,   z
+
+// minHeapBasedOnEndTime: [ ]
+// x: y
+// y: z
+// z: -1
+
+// minHeapBasedOnStartTime: []
+
+
 Output: [1, 2, -1]
 Explanation: The next interval of [2,3] is [3,4] having index ‘1’. 
 Similarly, the next interval of [3,4] is [5,6] having index ‘2’.
 There is no next interval for [5,6] hence we have ‘-1’.
+
 Example 2:
 Input: Intervals [[3,4], [1,5], [4,6]]
 Output: [2, -1, -1]
@@ -20,6 +32,9 @@ There is no next interval for [1,5] and [4,6].
 */
 
 const Heap = require('collections/heap')
+
+// select 1 interval, find from remaining intervals: smallest startTime >= endTime of curInterval
+// => minHeap (based on startTime)
 
 class Interval {
   constructor(startTime, endTime, index) {
@@ -30,38 +45,39 @@ class Interval {
 }
 
 function findNextIntervals(intervals) {
-  const minHeapOnStartTime = new Heap(
+  const minHeapBasedOnEndTime = new Heap(
+    [],
+    null,
+    (a, b) => b.endTime - a.endTime,
+  )
+  const minHeapBasedOnStartTime = new Heap(
     [],
     null,
     (a, b) => b.startTime - a.startTime,
   )
-  const minHeapOnEndTime = new Heap([], null, (a, b) => b.endTime - a.endTime)
 
   for (let i = 0; i < intervals.length; i++) {
     const [startTime, endTime] = intervals[i]
-    minHeapOnStartTime.add(new Interval(startTime, endTime, i))
-    minHeapOnEndTime.add(new Interval(startTime, endTime, i))
+    minHeapBasedOnEndTime.add(new Interval(startTime, endTime, i))
+    minHeapBasedOnStartTime.add(new Interval(startTime, endTime, i))
   }
 
-  const output = []
+  const output = Array(intervals.length).fill(-1)
 
-  while (minHeapOnEndTime.length) {
-    const curInterval = minHeapOnEndTime.pop()
-    // find the next interval for curInterval
+  while (minHeapBasedOnEndTime.length) {
+    const curInterval = minHeapBasedOnEndTime.pop()
 
-    if (minHeapOnStartTime.length === 0) {
-      output.push(-1)
-      continue
+    // GOAL: find the next interval for curInterval
+    // minHeapBasedOnStartTime.length === 0;
+    if (minHeapBasedOnStartTime.length === 0) break
+
+    while (minHeapBasedOnStartTime.peek().startTime < curInterval.endTime) {
+      minHeapBasedOnStartTime.pop()
     }
 
-    while (minHeapOnStartTime.peek().startTime < curInterval.endTime) {
-      minHeapOnStartTime.pop()
-    }
-
-    if (minHeapOnStartTime.length > 0) {
-      output.push(minHeapOnStartTime.pop().index)
-    } else {
-      output.push(-1)
+    // here: minHeapBasedOnStartTime.peek().startTime >= curInterval.endTime
+    if (minHeapBasedOnStartTime.length) {
+      output[curInterval.index] = minHeapBasedOnStartTime.pop().index
     }
   }
 
