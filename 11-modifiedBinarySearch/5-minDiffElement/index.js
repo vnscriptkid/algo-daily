a8226235042b2f87bb786a5f5d4fb29d2f09c770
Thanh Nguyen <vnscriptkid@gmail.com>
2021-10-28 20:15:21 +0700

    Solved Minimum Difference Element

--- 11-modifiedBinarySearch/5-minDiffElement/index.js
+++ 11-modifiedBinarySearch/5-minDiffElement/index.js
@@ -3,29 +3,26 @@
 // Given an array of numbers sorted in ascending order,
 // find the element in the array that has the minimum difference with the given ‘key’.
 
-function findMinDiffElement(arr, key) {
-  let minDiffNum = null
-  let minDiffNow = Infinity
+// Input: [1, 3, 8, 10, 15], key = 12
+//                   r   lm
+// Output: 10
 
+function findMinDiffElement(arr, key) {
   let left = 0,
     right = arr.length - 1
+
   while (left <= right) {
     let middle = left + Math.floor((right - left) / 2)
 
-    let diff = Math.abs(key - arr[middle])
+    if (key === arr[middle]) return arr[middle]
 
-    if (diff < minDiffNow) {
-      minDiffNow = diff
-      minDiffNum = arr[middle]
-    }
-
-    if (minDiffNow === 0) return minDiffNum
-
-    if (key > arr[middle]) left++
-    else right--
+    if (key > arr[middle]) left = middle + 1
+    else right = middle - 1
   }
 
-  return minDiffNum
+  return Math.abs(key - arr[left]) < Math.abs(key - arr[right])
+    ? arr[left]
+    : arr[right]
 }
 
 module.exports = {findMinDiffElement}
