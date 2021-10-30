6e2e20033e2eb96744406f4e8f8d91ccc06a7d52
Thanh Nguyen <vnscriptkid@gmail.com>
2021-10-30 13:23:03 +0700

    Solved reverse then invert matrix

--- 12-bitManipulation/5-reverseThenInvertMatrix/index.js
+++ 12-bitManipulation/5-reverseThenInvertMatrix/index.js
@@ -1,18 +1,38 @@
-// 0 0 1
+// Problem Statement #
+// Given a binary matrix representing an image, we want to flip the image horizontally, then invert it.
+
+// To flip an image horizontally means that each row of the image is reversed.
+// For example, flipping [0, 1, 1] horizontally results in [1, 1, 0].
+
+// To invert an image means that each 0 is replaced by 1, and each 1 is replaced by 0.
+// For example, inverting [1, 1, 0] results in [0, 0, 1].
+
+// Input: [
+//   [1,0,1],
+//   [1,1,1],
+//   [0,1,1]
+// ]
+
+// Output: [
+//   [0,1,0],
+//   [0,0,0],
+//   [0,0,1]
+// ]
+
+// [0,1,1]
+// [1,1,0]
+// [0,0,1]
 
 function reverseThenInvert(matrix) {
-  const n = matrix.length
+  // 1 2 3, 3 / 2 = 1.5 ~ 1
+  // 1 2 3 4, 4 / 2 = 2 , left = 0, left < length / 2
   for (let row of matrix) {
-    for (let i = 0; i < row.length / 2; i++) {
-      let j = n - 1 - i
-      if (i !== j) {
-        ;[row[i], row[j]] = [row[j], row[i]]
-        row[i] = 1 ^ row[i]
-        row[j] = 1 ^ row[j]
-      } else {
-        row[i] = 1 ^ row[i]
-      }
+    for (let left = 0; left < row.length / 2; left++) {
+      let right = row.length - 1 - left
+
+      ;[row[left], row[right]] = [row[right] ^ 1, row[left] ^ 1]
     }
   }
 }
+
 module.exports = {reverseThenInvert}
