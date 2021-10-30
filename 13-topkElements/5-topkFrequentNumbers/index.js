3fcc49ac0848d8a17144ef5030b8a8719a8325f7
Thanh Nguyen <vnscriptkid@gmail.com>
2021-10-30 14:23:40 +0700

    Top 'K' Frequent Numbers

--- 13-topkElements/5-topkFrequentNumbers/index.js
+++ 13-topkElements/5-topkFrequentNumbers/index.js
@@ -3,30 +3,34 @@
 // Given an unsorted array of numbers, find the top ‘K’ frequently occurring numbers in it.
 
 // Input: [1, 3, 5, 12, 11, 12, 11], K = 2
+//                               ^
+
+// req; { 1: 1, 3: 1, 5: 1, 12: 2, 11: 2 }
+//                                         ^
+// maxSize: 3
+// minHeap: { [12,2], [11,2]  }
+
 // Output: [12, 11]
 // Explanation: Both '11' and '12' apeared twice.
 
 const Heap = require('collections/heap')
 
 function findTopFrequent(nums, k) {
-  let freq = {}
-
+  const freq = {}
   for (let num of nums) {
     if (!(num in freq)) freq[num] = 0
     freq[num]++
   }
 
-  const heap = new Heap([], null, (a, b) => b[1] - a[1])
+  const minHeap = new Heap([], null, (a, b) => b[1] - a[1])
 
-  for (let num of Object.keys(freq)) {
-    heap.add([Number(num), freq[num]])
+  for (let key in freq) {
+    minHeap.add([Number(key), freq[key]])
 
-    if (heap.length > k) {
-      heap.pop()
-    }
+    if (minHeap.length === k + 1) minHeap.pop()
   }
 
-  return heap.content.map(item => item[0])
+  return minHeap.content.map(item => item[0])
 }
 
 module.exports = {findTopFrequent}
