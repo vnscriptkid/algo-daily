c689d2796135daef32c139b44f25f7ab310d9cd3
Thanh Nguyen <vnscriptkid@gmail.com>
2021-11-05 09:43:55 +0700

    Solved Count of Subset Sum

--- 15-01knapsack/5-countOfSubsetSum/bruteForce.js
+++ 15-01knapsack/5-countOfSubsetSum/bruteForce.js
@@ -1,31 +1,40 @@
 // Count of Subset Sum (hard) #
 // Given a set of positive numbers, find the total number of subsets whose sum is equal to a given number ‘S’.
 
+// Input: {1, 1, 2, 3}, S=4
+// Output: 3
+// The given set has '3' subsets whose sum is '4': {1, 1, 2}, {1, 3}, {1, 3}
+// Note that we have two similar sets {1, 3}, because we have two '1' in our input.
+
+// 1     1           _
+// 1   1    _      1     _
+// 2  2 _  2  _  2   _  2  _
+// 3 3 _
+
 function countSubsetSum(
   nums,
   target,
-  curIndex = 0,
+  numIndex = 0,
   curSum = 0,
   final = {count: 0},
 ) {
   // base case
   if (curSum === target) {
-    final.count += 1
-    return
-  } else if (curSum > target || curIndex === nums.length) return
+    final.count++
+  }
 
-  // 2 choices:
-  // choice 1: include cur num
-  countSubsetSum(nums, target, curIndex + 1, curSum + nums[curIndex], final)
-  // choice 2: not include
-  countSubsetSum(nums, target, curIndex + 1, curSum, final)
+  if (curSum >= target || numIndex === nums.length) {
+    return
+  }
+
+  // recursive
+  // consider nums[numIndex]
+  // Choice 1: take this number
+  countSubsetSum(nums, target, numIndex + 1, curSum + nums[numIndex], final)
+  // Choice 2: skip this number
+  countSubsetSum(nums, target, numIndex + 1, curSum, final)
 
   return final.count
 }
-
-// Input: {1, 1, 2, 3}, S=4
-// Output: 3
-// The given set has '3' subsets whose sum is '4': {1, 1, 2}, {1, 3}, {1, 3}
-// Note that we have two similar sets {1, 3}, because we have two '1' in our input.
 
 module.exports = {countSubsetSum}
