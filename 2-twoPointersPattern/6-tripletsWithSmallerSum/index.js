83dabcbfeb5401b7fed031d560e39ef889915198
Thanh Nguyen <vnscriptkid@gmail.com>
2021-11-15 00:32:22 +0700

    Solved Triplets with Smaller Sum

--- 2-twoPointersPattern/6-tripletsWithSmallerSum/index.js
+++ 2-twoPointersPattern/6-tripletsWithSmallerSum/index.js
@@ -8,25 +8,36 @@
 // Output: 2
 // Explanation: There are two triplets whose sum is less than the target: [-1, 0, 3], [-1, 0, 2]
 
+// [-1, 0, x, y, z, 2, 3]
+//  ^      @           $
+//  k      i           j
+
 function findTripletsWithSmallerSum(nums, target) {
+  // sort the array
+  // fix one number
+  // limit the range of 2 other numbers
+  // count = 0;
+  // start checking sum of 3
+  // 1. sum < target: count += j - i
+  // 2. sum >= target: move the $ pointer leftward
   nums.sort((a, b) => a - b)
 
   let count = 0
+  for (let first = 0; first < nums.length - 2; first++) {
+    let second = first + 1
+    let third = nums.length - 1
 
-  for (let first = 0; first < nums.length; first++) {
-    let left = first + 1,
-      right = nums.length - 1
-
-    while (left < right) {
-      let curSum = nums[first] + nums[left] + nums[right]
+    while (second < third) {
+      let curSum = nums[first] + nums[second] + nums[third]
 
       if (curSum < target) {
-        count += right - left
-        left++
-      } else right--
+        count = count + (third - second)
+        second++
+      } else {
+        third--
+      }
     }
   }
-
   return count
 }
 
