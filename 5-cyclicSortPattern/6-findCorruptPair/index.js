02c94c1a4c0fdd43b0eb5064ffb0f5f4b02a393e
Thanh Nguyen <vnscriptkid@gmail.com>
2021-11-17 18:56:04 +0700

    Solved Find the Corrupt Pair

--- 5-cyclicSortPattern/6-findCorruptPair/index.js
+++ 5-cyclicSortPattern/6-findCorruptPair/index.js
@@ -4,35 +4,36 @@
 // The array originally contained all the numbers from 1 to ‘n’, but due to a data error,
 // one of the numbers got duplicated which also resulted in one number going missing. Find both these numbers.
 
+//        [1, 2, 3, 4, 5]
 // Input: [3, 1, 2, 5, 2]
 // Output: [2, 4]
 // Explanation: '2' is duplicated and '4' is missing.
-
-// 1  2  3  4  5
-// 3  1  2  5  2
-//          *
-// 1  2  3  2  5
 
 // Input: [3, 1, 2, 3, 6, 4]
 // Output: [3, 5]
 // Explanation: '3' is duplicated and '5' is missing.
 
-// [1, 2, 3, 4, 3, 6]
-//              ^
+//  0  1  2  3  4
+// [1, 2, 3, 2, 5]   number n should be at index (n - 1)
+//           ^
 
 function findCorruptPair(arr) {
   let i = 0
   while (i < arr.length) {
     let curNum = arr[i]
     let correctIdx = curNum - 1
+
+    // goal: bring curNum to it's correct position
     if (i !== correctIdx && arr[correctIdx] !== correctIdx + 1) {
       ;[arr[i], arr[correctIdx]] = [arr[correctIdx], arr[i]]
     } else i++
   }
 
   for (let i = 0; i < arr.length; i++) {
-    if (arr[i] !== i + 1) return [arr[i], i + 1]
+    if (arr[i] !== i + 1) return [arr[i], i + 1].sort()
   }
+
+  throw new Error('oops! it should not come here')
 }
 
 module.exports = {findCorruptPair}
