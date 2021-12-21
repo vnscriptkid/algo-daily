913e32d94bc68d796e98bd55c5445410793e8f5f
Thanh Nguyen <vnscriptkid@gmail.com>
2021-12-21 09:25:49 +0700

    Solved Cycle in a Circular Array

--- 3-slowFastPointers/7-cycleInCircularArray/index.js
+++ 3-slowFastPointers/7-cycleInCircularArray/index.js
@@ -1,42 +1,71 @@
-// [1, 2, -1, 2, 2]
+// Cycle in a Circular Array
+// We are given an array containing positive and negative numbers.
+// Suppose the array contains a number ‘M’ at a particular index.
+// Now, if ‘M’ is positive we will move forward ‘M’ indices and if ‘M’ is negative move backwards ‘M’ indices.
+// You should assume that the array is circular which means two things:
+
+// (1) If, while moving forward, we reach the end of the array, we will jump to the first element to continue the movement.
+// If, while moving backward, we reach the beginning of the array, we will jump to the last element to continue the movement.
+
+// Write a method to determine if the array has a cycle.
+// (2) The cycle should have more than one element and
+// (3) should follow one direction which means the cycle should not contain both forward and backward movements.
+
+//         0  1   2  3  4
+// Input: [1, 2, -1, 2, 2]  (3 + 2 = 5 % 5 = 0)
+//                   ^
+//         $
+// Output: true
+// Explanation: The array has a cycle among indices: 0 -> 1 -> 3 -> 0
+
+// Input: [2, 2, -1, 2]
+// Output: true
+// Explanation: The array has a cycle among indices: 1 -> 3 -> 1
+
+// Input: [2, 1, -1, -2]
+// Output: false
+// Explanation: The array does not have any cycle.
+
+//  0  1   2
+// [1, 3, -1]
 
 function isCircular(arr) {
-  Number.prototype.next = function (curDirection) {
-    const curIdx = this
+  function nextOf(idx, isForward) {
+    let nextIdx = (idx + arr[idx]) % arr.length
 
-    const isForward = arr[curIdx] > 0
+    if (nextIdx === idx) return -1
 
-    if (isForward !== curDirection) return -1
+    let isNowForward = arr[nextIdx] > 0
 
-    const nextIdx = (curIdx + arr[curIdx]) % arr.length
+    if (isForward !== isNowForward) return -1
 
     return nextIdx
   }
 
-  // start at each number
   for (let i = 0; i < arr.length; i++) {
-    // is there cycle starting at i
+    // must check if there is a cycle starting at every number (loop)
+
     let slow = i,
       fast = i
-    // 1. no changing at direction
-    // 2. slow meets fast
     let isForward = arr[i] > 0
 
     while (true) {
-      slow = slow.next(isForward)
+      slow = nextOf(slow, isForward)
+      fast = nextOf(fast, isForward)
 
-      fast = fast.next(isForward)
-
-      // changing direction ?
       if (fast === -1) break
 
-      fast = fast.next(isForward)
+      fast = nextOf(fast, isForward)
+
       if (fast === -1) break
 
       if (slow === fast) return true
     }
   }
+  // at least 1 => true
+  // no cycle starting at current number (keep checking)
 
+  // return false
   return false
 }
 
