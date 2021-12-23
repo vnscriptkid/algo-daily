2f0c1664986e8ae4e06424f51f8c4e3e6aa36756
Thanh Nguyen <vnscriptkid@gmail.com>
2021-12-23 10:41:40 +0700

    Minimum Meeting Rooms

--- 4-mergeIntervals/5-minimumMeetingRooms/index.js
+++ 4-mergeIntervals/5-minimumMeetingRooms/index.js
@@ -1,39 +1,47 @@
 // Minimum Meeting Rooms
 
-const Heap = require('collections/heap')
-
 // Given a list of intervals representing the start and end time of ‘N’ meetings,
 // find the minimum number of rooms required to hold all the meetings.
 
-// Meetings: [[4,5], [2,3], [2,4], [3,5]]
+// Meetings: [[4,5], [2,3], [2,4], [3,5]] => [ [2,3], [2,4], [3,5], [4,5] ]
 // Output: 2
 // Explanation: We will need one room for [2,3] and [3,5], and another room for [2,4] and [4,5].
 
-// Here is a visual representation of Example 4:
+// 1  2  3  4  5  6
+//    [  ]
+//    [     ]
+//       [     ]
+//          [  ]
+
+// (A, B, C) => D (minHeapBasedOnEndTime)
+
+const Heap = require('collections/heap')
 
 function findMinMeetingRooms(meetings) {
-  // sort by startTime
   meetings.sort((a, b) => a[0] - b[0])
 
   const concurrentMeetings = new Heap([], null, (a, b) => b[1] - a[1])
-  let minRooms = 1
 
-  for (let curMeeting of meetings) {
-    // remove any meeting that has ended at the time curMeeting starts
+  let numOfRooms = 0
+
+  for (let meeting of meetings) {
+    let [startTime] = meeting
+
+    // how many meetings are happening at the time curMeeting starts
+
     while (
       concurrentMeetings.length &&
-      concurrentMeetings.peek()[1] <= curMeeting[0]
+      startTime >= concurrentMeetings.peek()[1]
     ) {
       concurrentMeetings.pop()
     }
 
-    concurrentMeetings.add(curMeeting)
+    concurrentMeetings.add(meeting)
 
-    // size of concurrentMeetings will be min number of rooms needed
-    minRooms = Math.max(concurrentMeetings.length, minRooms)
+    numOfRooms = Math.max(numOfRooms, concurrentMeetings.length)
   }
 
-  return minRooms
+  return numOfRooms
 }
 
 module.exports = {findMinMeetingRooms}
