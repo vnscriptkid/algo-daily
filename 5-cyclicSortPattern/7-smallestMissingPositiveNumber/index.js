a3bbff149206ad09ae7fec97be114be27b1d2ff6
Thanh Nguyen <vnscriptkid@gmail.com>
2021-12-24 16:26:18 +0700

    Solved Find the Smallest Missing Positive Number

--- 5-cyclicSortPattern/7-smallestMissingPositiveNumber/index.js
+++ 5-cyclicSortPattern/7-smallestMissingPositiveNumber/index.js
@@ -1,34 +1,38 @@
 // Find the Smallest Missing Positive Number (medium) #
 // Given an unsorted array containing numbers, find the smallest missing positive number in it.
 
-// [-10, -7, 30, 20, 3, 2, 5, 1]
-
+// [1, 2, 3, 4, 5, 6, 7, 8]
 // [1, 2, 3, 20, 5, -7, 30, -10]
-//                               ^
+//           ^
+// 4
+
+// [1, 2, 3, 4, 5]
+// [5, 3, 4, 1, 2] => 6
 
 function findSmallestMissingPositiveNumber(arr) {
-  // first iteration: bring numbers that belongs to [1, n], to its correct position
-  let n = arr.length
+  // first iteration: bring curNumber to its correct idx (if number in range of [1, arr.length])
   let i = 0
-  while (i < n) {
+  while (i < arr.length) {
     let curNum = arr[i]
-    if (curNum < 1 || curNum > n) {
-      i++
-      continue
-    }
-
     let correctIdx = curNum - 1
-    if (i !== correctIdx) {
+
+    if (
+      i !== correctIdx &&
+      curNum >= 1 &&
+      curNum <= arr.length &&
+      arr[correctIdx] !== correctIdx + 1
+    ) {
       ;[arr[i], arr[correctIdx]] = [arr[correctIdx], arr[i]]
     } else i++
   }
 
-  // second iteration: find first number that is not at its correct position
-  for (let [i, num] of arr.entries()) {
-    if (num !== i + 1) return i + 1
+  // second iteration: find the first number that is not at its correct index
+  for (let [curIdx, curNum] of arr.entries()) {
+    let numShouldBeHere = curIdx + 1
+    if (curNum !== numShouldBeHere) return numShouldBeHere
   }
 
-  return n + 1
+  return arr.length + 1
 }
 
 module.exports = {findSmallestMissingPositiveNumber}
