13406684f8e8f1dd200ed6a10e44e2551d0a439c
Thanh Nguyen <vnscriptkid@gmail.com>
2021-12-30 22:11:16 +0700

    Solved Count Paths for a Sum

--- 8-depthFirstSearch/5-countPathsForSum/index.js
+++ 8-depthFirstSearch/5-countPathsForSum/index.js
@@ -1,3 +1,21 @@
+// Count Paths for a Sum
+
+// Given a binary tree and a number ‘S’, find all paths in the tree such that
+// the sum of all the node values of each path equals ‘S’.
+// Please note that the paths can start or end at any node but
+//  all paths must follow direction from parent to child (top to bottom).
+
+// S = 11
+//     12
+//    /  \
+//   7    1
+//  /    / \
+// 4    10  5
+
+// []
+
+// [7, 4], [1, 10]
+
 class Node {
   constructor(val, left = null, right = null) {
     this.val = val
@@ -7,21 +25,23 @@ class Node {
 }
 
 function countPaths(root, target, curPath = [], count = {current: 0}) {
-  // visit cur node
+  if (!root) return 0
+
+  // visit the current node
   curPath.push(root.val)
 
-  // calculate all sums ending at curNode
-  let sum = 0
+  // check sum of all paths that ends at current node
+  let curSum = 0
   for (let i = curPath.length - 1; i >= 0; i--) {
-    sum += curPath[i]
-    if (sum === target) count.current++
+    curSum += curPath[i]
+
+    if (curSum === target) count.current++
   }
 
-  // recursive traversal
+  // traverse down the tree
   for (let child of [root.left, root.right]) {
     if (child) {
       countPaths(child, target, curPath, count)
-      // backtrack
       curPath.pop()
     }
   }
