3ffd200dabd6cfa3c598723e59f723f7e1584095
Thanh Nguyen <vnscriptkid@gmail.com>
2022-01-04 10:30:00 +0700

    Solved Kth Largest Number in a Stream

--- 13-topkElements/7-kthLargestNumberStream/index.js
+++ 13-topkElements/7-kthLargestNumberStream/index.js
@@ -1,3 +1,5 @@
+// Kth Largest Number in a Stream
+
 // Design a class to efficiently find the Kth largest element in a stream of numbers.
 
 // The class should have the following two things:
@@ -5,14 +7,22 @@
 // The constructor of the class should accept an integer array containing initial numbers from the stream and an integer ‘K’.
 // The class should expose a function add(int num) which will store the given number and return the Kth largest number.
 
+// Input: [3, 1, 5, 12, 2, 11], K = 4
+//                         ^
+// ((3), 5, 12, 11)
+
+// 1. Calling add(6) should return '5'.
+// 2. Calling add(13) should return '6'.
+// 2. Calling add(4) should still return '6'.
+
 const Heap = require('collections/heap')
 
 class Stream {
-  constructor(initial = [], k) {
+  constructor(nums, k) {
     this.k = k
     this.minHeap = new Heap([], null, (a, b) => b - a)
-    // init
-    for (let num of initial) {
+
+    for (let num of nums) {
       this.add(num)
     }
   }
@@ -20,7 +30,9 @@ class Stream {
   add(num) {
     this.minHeap.add(num)
 
-    if (this.minHeap.length > this.k) this.minHeap.pop()
+    if (this.minHeap.length === this.k + 1) {
+      this.minHeap.pop()
+    }
 
     return this.minHeap.peek()
   }
