046ec4428be27f212661d98dcb5dafca9c496aee
Thanh Nguyen <vnscriptkid@gmail.com>
2022-02-05 00:29:56 +0700

    Solved Number Range

--- 11-modifiedBinarySearch/9-numberRange/index.js
+++ 11-modifiedBinarySearch/9-numberRange/index.js
@@ -8,45 +8,47 @@
 // Input: [4, 6, 6, 6, 9], key = 6
 // Output: [1, 3]
 
+// [4, 6, 6, 6, 9]
+//  l     m     r
+
 // Input: [1, 3, 8, 10, 15], key = 12
 // Output: [-1, -1]
 
-function findLastKey(arr, key, leftMost) {
-  let finalIdx = -1
-
+function findIdx(arr, key, findFirst) {
   let left = 0,
     right = arr.length - 1
+
+  let foundIdx = -1
 
   while (left <= right) {
     let middle = Math.floor(left + (right - left) / 2)
 
     if (arr[middle] === key) {
-      finalIdx = middle
-
-      if (leftMost) {
+      foundIdx = middle
+      if (findFirst) {
         right = middle - 1
       } else {
-        // rightMost
+        // findLast
         left = middle + 1
       }
-    } else if (key > arr[middle]) {
-      left = middle + 1
-    } else {
-      // key < arr[middle]
+    } else if (arr[middle] > key) {
       right = middle - 1
+    } else {
+      // arr[middle] < key
+      left = middle + 1
     }
   }
 
-  return finalIdx
+  return foundIdx
 }
 
 function findNumberRange(arr, key) {
-  let [left, right] = [-1, -1]
+  let [first, last] = [-1, -1]
 
-  left = findLastKey(arr, key, true)
-  right = findLastKey(arr, key, false)
+  first = findIdx(arr, key, true)
+  last = findIdx(arr, key, false)
 
-  return [left, right]
+  return [first, last]
 }
 
 module.exports = {findNumberRange}
