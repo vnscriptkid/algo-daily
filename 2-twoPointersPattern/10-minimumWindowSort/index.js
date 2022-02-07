94dc2f0beab72aacbc207f2d9230b28d8c7d1117
Thanh Nguyen <vnscriptkid@gmail.com>
2022-02-07 21:29:04 +0700

    Solved Min Window Sort

--- 2-twoPointersPattern/10-minimumWindowSort/index.js
+++ 2-twoPointersPattern/10-minimumWindowSort/index.js
@@ -1,35 +1,78 @@
+// Min Window Sort
+
+// Given an array, find the length of the smallest subarray in it which when sorted will sort the whole array.
+
+// Input: [1, 2, 5, 3, 7, 10, 9, 12]
+// Output: 5
+// Explanation: We need to sort only the subarray [5, 3, 7, 10, 9] to make the whole array sorted
+
+// [1, 2, 5, 3, 7, 10, 9, 12]
+//        ^            $
+
+// [1, 2, 3, 5, 7, 9. 10, 12]
+//        ^            $
+
+/*********** */
+
+// Input: [1, 3, 2, 0, -1, 7, 10]
+// Output: 5
+// Explanation: We need to sort only the subarray [1, 3, 2, 0, -1] to make the whole array sorted
+
+// [1, 3, 2, 0, -1, 7, 10]
+//     l         h
+
+// max
+// min
+
+// [1, -1, 0, 2, 3, 7, 10]
+//     ^         $
+
 function findSmallestWindow(arr) {
-  // find first num out of order from left, call left
-  let left = 0
-  while (arr[left + 1] !== undefined && arr[left] < arr[left + 1]) left++
-
-  if (left === arr.length - 1) return 0
-
-  // find first num out of order from right, call right
-  let right = arr.length - 1
-  while (arr[right - 1] !== undefined && arr[right - 1] < arr[right]) right--
-
-  if (right === 0) return 0
-
-  // find min,max between [left - 1, right + 1]
-  let i = left
-  let min = Infinity
-  let max = -Infinity
-  while (i <= right) {
-    if (arr[i] < min) min = arr[i]
-    if (arr[i] > max) max = arr[i]
-    i++
+  // go from the left, find the first out-of-order number (low)
+  let low = 0
+  while (low + 1 < arr.length && arr[low] < arr[low + 1]) {
+    low++
   }
 
-  // from left - 1, go to the left find first num less than min
-  let finalLeft = left
-  while (finalLeft - 1 >= 0 && min < arr[finalLeft - 1]) finalLeft--
+  // go from the right, find the first out-of-order number (high)
+  let high = arr.length - 1
+  while (high - 1 >= 0 && arr[high - 1] < arr[high]) {
+    high--
+  }
 
-  // from right + 1, go to the right, find first num greater than max
-  let finalRight = right
-  while (finalRight + 1 < arr.length && max > arr[finalRight + 1]) finalRight++
+  // 1 2 3 4 5 6 7
+  if (low >= high) return 0
 
-  return finalRight - finalLeft + 1
+  // find max, min between low and high idx
+  let min = Infinity
+  let max = -Infinity
+  for (let i = low; i <= high; i++) {
+    min = Math.min(min, arr[i])
+    max = Math.max(max, arr[i])
+  }
+
+  let smallestWindow = high - low + 1
+
+  // go from low - 1, all the way to the left
+  // a b c | x y z | d e f
+  let cur = low - 1
+  // include number greater than min
+  // stop when curNum < min
+  while (cur >= 0 && arr[cur] > min) {
+    smallestWindow++
+    cur--
+  }
+
+  cur = high + 1
+  while (cur < arr.length && arr[cur] < max) {
+    smallestWindow++
+    cur++
+  }
+  // go from high + 1, all the way to the right
+  // include number less than max
+  // stop when curNum > max
+
+  return smallestWindow
 }
 
 module.exports = {findSmallestWindow}
