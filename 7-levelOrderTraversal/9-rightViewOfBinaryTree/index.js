2eb5c7565e65bb533dc0c8b54300af6d5458bd2c
Thanh Nguyen <vnscriptkid@gmail.com>
2022-02-08 21:35:24 +0700

    Solved Right view of binary tree

--- 7-levelOrderTraversal/9-rightViewOfBinaryTree/index.js
+++ 7-levelOrderTraversal/9-rightViewOfBinaryTree/index.js
@@ -1,3 +1,18 @@
+// Right view of binary tree
+
+// Given a binary tree, return an array containing nodes in its right view.
+// The right view of a binary tree is the set of nodes visible when the tree is seen from the right side.
+
+//       1
+//      / \
+//     2   3
+//    / \ / \
+//   4  5 6  7
+
+// [ 4, 5, 6, 7 ]
+// levelSize = 0
+// 3
+
 class Node {
   constructor(val, left = null, right = null) {
     this.val = val
@@ -8,22 +23,24 @@ class Node {
 
 function rightView(root) {
   const queue = [root]
+  // queue to go level by level
 
-  const result = []
+  let result = []
 
   while (queue.length) {
+    // loop: process 1 level in 1 iteration
     let levelSize = queue.length
 
     while (levelSize > 0) {
-      const {left, right, val} = queue.shift()
+      levelSize--
 
-      if (levelSize === 1) result.push(val)
-
-      for (let child of [left, right]) {
+      const node = queue.shift() // dequeue
+      // push new nodes to queue
+      for (let child of [node.left, node.right]) {
         if (child) queue.push(child)
       }
-
-      levelSize--
+      // check if we're at last of 1 level => push to result
+      if (levelSize === 0) result.push(node.val)
     }
   }
 
