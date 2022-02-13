b2cd5ebc32a21ffbc5addab6b9c33e71696853d6
Thanh Nguyen <vnscriptkid@gmail.com>
2022-02-13 21:51:39 +0700

    Solved Search in Rotated Array

--- 11-modifiedBinarySearch/10-rotationCount/index.js
+++ 11-modifiedBinarySearch/10-rotationCount/index.js
@@ -1,24 +1,32 @@
-// case 1: 8 9 1 2 4 5 6 7
+// Search in Rotated Array (medium) #
+// Given an array of numbers which is sorted in ascending order and also rotated by some arbitrary number,
+// find if a given ‘key’ is present in it.
 
-// case 2: 4 5 6 7 8 9 1 2
+// Write a function to return the index of the ‘key’ in the rotated array. If the ‘key’ is not present, return -1.
+// You can assume that the given array does not have any duplicates.
+
+// Example 1:
+// Input: [10, 15, 1, 3, 8], key = 15
+// Output: 1
+// Explanation: '15' is present in the array at index '1'.
+
+// Example 2:
+// Input: [4, 5, 7, 9, 10, -1, 2], key = 10
+// Output: 4
+// Explanation: '10' is present in the array at index '4'.
 
 function countRotations(arr) {
-  // find min idx
+  // find idx of smallest number
   let left = 0,
     right = arr.length - 1
-
   while (left < right) {
-    let middle = Math.floor(left + (right - left) / 2)
+    let middle = left + Math.floor((right - left) / 2)
 
-    if (middle < right && arr[middle] > arr[middle + 1]) return middle + 1
+    if (middle - 1 >= left && arr[middle - 1] > arr[middle]) return middle
+    if (middle + 1 <= right && arr[middle] > arr[middle + 1]) return middle + 1
 
-    if (left < middle && arr[middle - 1] > arr[middle]) return middle
-
-    if (arr[left] < arr[middle]) {
-      left = middle + 1
-    } else {
-      right = middle - 1
-    }
+    if (arr[left] <= arr[middle]) left = middle + 1
+    else right = middle - 1
   }
 
   return 0
