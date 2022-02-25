ba87a062043b3fe0f08ac211766bb7fafdea0eda
Thanh Nguyen <vnscriptkid@gmail.com>
2022-02-25 11:23:22 +0700

    Solved String Anagrams

--- 1-slidingWindowPattern/10-stringAnagrams/index.js
+++ 1-slidingWindowPattern/10-stringAnagrams/index.js
@@ -13,52 +13,55 @@
 
 // String="p p a q q p", Pattern="pq"
 
-// Input: String="abbcabc", Pattern="abc"
+// Input: String="a b b c a b c", Pattern="abc"
 // Output: [2, 3, 4]
 // Explanation: The three anagrams of the pattern in the given string are "bca", "cab", and "abc".
-// a b b c a b c
-//         ^
-//             $
 
-// matches: 3
-// window: {  a: 1, b: 1, c: 1 }
-// pattern: { a: 1, b: 1, c: 1 }
+// a b b c a b c
+//           ^
+//               $
+
+// curMatch: 3
+// { a: 0 ; b: 0; c : 0 }
 // result: [2, 3, 4]
 
 function findAnagrams(str, pattern) {
   const patternMap = {}
 
+  // build the map
   for (let char of pattern) {
     if (!(char in patternMap)) patternMap[char] = 0
     patternMap[char]++
   }
 
-  let needToMatch = Object.keys(patternMap).length
-
-  const result = []
+  // slide our window
+  let numOfMatch = 0
   let windowStart = 0
-  let curMatch = 0
+  const result = []
   for (let windowEnd = 0; windowEnd < str.length; windowEnd++) {
     let curChar = str[windowEnd]
+
     if (curChar in patternMap) {
       patternMap[curChar]--
 
-      if (patternMap[curChar] === 0) curMatch++
-      else if (patternMap[curChar] === -1) curMatch--
+      if (patternMap[curChar] === 0) numOfMatch++
+      else if (patternMap[curChar] === -1) numOfMatch--
     }
 
-    if (curMatch === needToMatch) result.push(windowStart)
+    if (numOfMatch === Object.keys(patternMap).length) {
+      result.push(windowStart)
+    }
 
-    let windowSize = windowEnd - windowStart + 1
+    const windowSize = windowEnd - windowStart + 1
 
     if (windowSize === pattern.length) {
-      // shrink down
+      // shrink it down
       let firstChar = str[windowStart]
       windowStart++
 
       if (firstChar in patternMap) {
-        if (patternMap[firstChar] === 0) curMatch--
-        else if (patternMap[firstChar] === -1) curMatch++
+        if (patternMap[firstChar] === 0) numOfMatch--
+        else if (patternMap[firstChar] === -1) numOfMatch++
 
         patternMap[firstChar]++
       }
