9076fe13ff7d9fd2a82dcc8dd576beedbd8eaaa2
Thanh Nguyen <vnscriptkid@gmail.com>
2022-03-14 00:24:33 +0700

    Rearrange String K Distance Apart

--- 13-topkElements/12-rearrangeStringKDistanceApart/index.js
+++ 13-topkElements/12-rearrangeStringKDistanceApart/index.js
@@ -3,8 +3,9 @@
 // such that the same characters are at least ‘K’ distance apart from each other.
 
 // Example 1:
-
 // Input: "mmpp", K=2
+// m_m
+// p_p
 // Output: "mpmp" or "pmpm"
 // Explanation: All same characters are 2 distance apart.
 // Example 2:
@@ -26,35 +27,40 @@
 const Heap = require('collections/heap')
 
 function rearrangeString(str, k) {
+  // count freq of each char
   const freq = {}
-
   for (let char of str) {
     if (!(char in freq)) freq[char] = 0
     freq[char]++
   }
-
+  // put char with freq into maxHeap
   const maxHeap = new Heap([], null, (a, b) => a[1] - b[1])
   for (let char in freq) {
     maxHeap.add([char, freq[char]])
   }
-
+  // need a queue, store at max k items
   const queue = []
-  let finalStr = ''
+  // take out one by one from maxHeap, put into cur str
+  let curStr = ''
   while (maxHeap.length) {
-    const curItem = maxHeap.pop()
+    const item = maxHeap.pop()
 
-    finalStr += curItem[0]
+    curStr += item[0]
 
-    queue.push([curItem[0], curItem[1] - 1])
+    // push item into queue
+    queue.push([item[0], item[1] - 1])
 
+    // consider adding back item to maxHeap when queue is of size k
     if (queue.length === k) {
-      const itemBack = queue.shift()
+      const candidate = queue.shift()
 
-      if (itemBack[1] > 0) maxHeap.add(itemBack)
+      if (candidate[1] > 0) {
+        maxHeap.add(candidate)
+      }
     }
   }
-
-  return finalStr.length === str.length ? finalStr : ''
+  // return curStr
+  return curStr.length !== str.length ? '' : curStr
 }
 
 module.exports = {rearrangeString}
