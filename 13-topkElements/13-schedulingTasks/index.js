c85329ab7705d5b19ecf46d6bad01913d5a4f4a2
Thanh Nguyen <vnscriptkid@gmail.com>
2022-03-15 23:36:17 +0700

    Solved Scheduling Tasks

--- 13-topkElements/13-schedulingTasks/index.js
+++ 13-topkElements/13-schedulingTasks/index.js
@@ -1,44 +1,68 @@
+// Scheduling Tasks (hard) #
+// You are given a list of tasks that need to be run, in any order, on a server.
+// Each task will take one CPU interval to execute but once a task has finished,
+// it has a cooling period during which it can’t be run again.
+// If the cooling period for all tasks is ‘K’ intervals,
+// find the minimum number of CPU intervals that the server needs to finish all tasks.
+
 const Heap = require('collections/heap')
 
+// If at any time the server can’t execute any task then it must stay idle.
+
+// Example 1:
+
+// Input: [a, a, a, b, c, c], K=2
+// Output: 7
+// Explanation: a -> c -> b -> a -> c -> idle -> a
+
+//
+
+// Example 2:
+
+// Input: [a, b, a], K=3
+// Output: 5
+// Explanation: a -> b -> idle -> idle -> a
+
 function scheduleTasks(tasks, k) {
-  // feed tasks into freq map
+  // count freq of each task
   const freq = {}
   for (let task of tasks) {
     if (!(task in freq)) freq[task] = 0
     freq[task]++
   }
-  // feed tasks into a max heap
+  // add [ task, taskFreq ] into maxHeap
   const maxHeap = new Heap([], null, (a, b) => a[1] - b[1])
   for (let task in freq) {
     maxHeap.add([task, freq[task]])
   }
-  // init a queue to simulate waiting time
-  let minCpuIntervals = 0
-  // take one by one from heap
-  while (maxHeap.length > 0) {
+  // tasksCount
+  let tasksCount = 0
+  while (maxHeap.length) {
+    // process in batch of k + 1
+    let numOfTasksThisBatch = k + 1
     const queue = []
-    let n = k + 1
+    while (numOfTasksThisBatch > 0 && maxHeap.length) {
+      const [task, taskFreq] = maxHeap.pop()
 
-    while (n > 0 && maxHeap.length) {
-      const [task, count] = maxHeap.pop()
-      if (count > 1) {
-        queue.push([task, count - 1])
+      if (taskFreq - 1 > 0) {
+        queue.push([task, taskFreq - 1])
       }
-      minCpuIntervals++
-      n--
-    }
 
-    while (queue.length) {
-      const [task, count] = queue.shift()
-      maxHeap.add([task, count])
+      numOfTasksThisBatch--
     }
+    // do we have next batch?
+    if (queue.length) {
+      // yes: tasksCount += k + 1;
+      tasksCount += k + 1
 
-    if (maxHeap.length > 0) {
-      minCpuIntervals += n
+      queue.forEach(item => maxHeap.add(item))
+    } else {
+      // no: tasksCount += numOfTasksUsedInThisBatch
+      tasksCount += k + 1 - numOfTasksThisBatch
     }
   }
-  // return minCpuIntervals
-  return minCpuIntervals
+  // return tasksCount
+  return tasksCount
 }
 
 module.exports = {scheduleTasks}
--- 13-topkElements/13-schedulingTasks/test.js
+++ 13-topkElements/13-schedulingTasks/test.js
@@ -7,9 +7,9 @@ test('it works 1', () => {
   expect(result).toEqual(7)
 })
 
-// test("it works 1", () => {
-//   const tasks = ["a", "b", "a"],
-//     K = 3;
-//   const result = scheduleTasks(tasks, K);
-//   expect(result).toEqual(5);
-// });
+test('it works 1', () => {
+  const tasks = ['a', 'b', 'a'],
+    K = 3
+  const result = scheduleTasks(tasks, K)
+  expect(result).toEqual(5)
+})
