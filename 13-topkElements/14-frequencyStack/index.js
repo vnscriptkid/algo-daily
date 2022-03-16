b6271f45a3535ac946eee6a81e7dd2480bf16b0b
Thanh Nguyen <vnscriptkid@gmail.com>
2022-03-16 23:22:34 +0700

    Solved Frequency Stack

--- 13-topkElements/14-frequencyStack/index.js
+++ 13-topkElements/14-frequencyStack/index.js
@@ -1,49 +1,37 @@
+// Frequency Stack (hard)
 // Design a class that simulates a Stack data structure, implementing the following two operations:
-
-// push(int num): Pushes the number ‘num’ on the stack.
-// pop(): Returns the most frequent number in the stack. If there is a tie, return the number which was pushed later.
-
-// 1 2 3 2 1 2 5
-//             ^
-
-// heap: [(1:1), (2:1), (3:1), (2:2), (1:2), (5:1)]
-// map: { 1:2, 2:2, 3:1, 5:1 }
 
 const Heap = require('collections/heap')
 
+// push(int num): Pushes the number ‘num’ on the stack.
+// pop(): Returns the most frequent number in the stack. If there is a tie, return the number which was pushed later.
+// Example:
+
+// After following push operations: push(1), push(2), push(3), push(2), push(1), push(2), push(5)
+
+// 1. pop() should return 2, as it is the most frequent number
+// 2. Next pop() should return 1
+
 class Stack {
   constructor() {
-    this.data = new Heap([], null, (a, b) => {
-      // [num, freq, order]
-      return a[1] === b[1] ? a[2] - b[2] : a[1] - b[1]
+    this.freq = {} // num: numFreq
+    // [ num, numFreq, order ]
+    this.maxHeap = new Heap([], null, (a, b) => {
+      return a[1] !== b[1] ? a[1] - b[1] : a[2] - b[2]
     })
-    this.count = 0
-    this.freq = {}
+    this.order = 0
   }
 
   push(num) {
     if (!(num in this.freq)) this.freq[num] = 0
     this.freq[num]++
 
-    this.data.add([num, this.freq[num], this.count++])
+    this.maxHeap.add([num, this.freq[num], this.order++])
   }
 
   pop() {
-    if (this.data.length === 0) return null
-
-    const [num] = this.data.pop()
-
-    this.freq[num]--
-    if (this.freq[num] === 0) delete this.freq[num]
-
-    return num
+    return this.maxHeap.pop()[0]
   }
 }
 
 module.exports = {Stack}
-
-// 1 2 3 1
-
-//{ (1:2), (2:1), (3:1) }
-
-// { 1:2, 2:1, 3:1 }
